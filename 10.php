87aaeb696d61482a8469c76e20452ceb15b4c793
flannan <flannan383@gmail.com>
2019-03-26 12:03:09 +0300

    problem 10

---
+++ 10.php
@@ -0,0 +1,29 @@
+<?php /** @noinspection ForgottenDebugOutputInspection */
+
+
+$strings = [
+    //Пример правильных выражений:
+    '123456',
+    '234567',
+    //Пример неправильных выражений:
+    '1234567',
+    '12345'
+];
+
+
+echo 'PHP comparison' . PHP_EOL;
+foreach ($strings as $string) {
+    $isCorrect = ctype_digit($string);
+    if ($isCorrect) {
+        $isCorrect = (((int)$string >= 1e5) && ((int)$string < 1e6));
+    }
+    var_export($isCorrect);
+    echo PHP_EOL;
+}
+
+echo 'regex comparison' . PHP_EOL;
+$regex = '#^[1-9]\d{5}$#';
+
+foreach ($strings as $string) {
+    echo preg_match($regex, $string) . PHP_EOL;
+}
