4dd3aa80cbedbd2d501f7f9f3a9d993e3722e7f5
flannan <flannan383@gmail.com>
2019-03-26 14:52:48 +0300

    problem 4 refactored

--- 4.php
+++ 4.php
@@ -1,55 +1,56 @@
 <?php /** @noinspection ForgottenDebugOutputInspection */
 
-//Пример правильных выражений:
-$sampleURL1 = 'http://www.zcontest.ru';
-$sampleURL2 = 'http://zcontest.ru';
-$sampleURL3 = 'http://zcontest.com';
-$sampleURL4 = 'https://zcontest.ru';
-$sampleURL5 = 'https://sub.zcontest-ru.com:8080';
-$sampleURL6 = 'http://zcontest.ru/dir%201/dir_2/program.ext?var1=x&var2=my%20value';
-$sampleURL7 = 'zcon.com/index.html#bookmark';
-
-//Пример неправильных выражений:
-$sampleURL8 = 'Just Text.';
-$sampleURL9 = 'http://a.com';
-$sampleURL10 = 'http://www.domain-.com';
-
-require_once __DIR__ . '/4.validateURL.php';
+$sampleURLs = [
+    //Пример правильных выражений:
+    'http://www.zcontest.ru',
+    'http://zcontest.ru',
+    'http://zcontest.com',
+    'https://zcontest.ru',
+    'https://sub.zcontest-ru.com:8080',
+    'http://zcontest.ru/dir%201/dir_2/program.ext?var1=x&var2=my%20value',
+    'zcon.com/index.html#bookmark',
+    //Пример неправильных выражений:
+    'Just Text.',
+    'http://a.com',
+    'http://www.domain-.com'
+];
 
 echo 'PHP comparison' . PHP_EOL;
-var_export(validateURL($sampleURL1));
-echo PHP_EOL;
-var_export(validateURL($sampleURL2));
-echo PHP_EOL;
-var_export(validateURL($sampleURL3));
-echo PHP_EOL;
-var_export(validateURL($sampleURL4));
-echo PHP_EOL;
-var_export(validateURL($sampleURL5));
-echo PHP_EOL;
-var_export(validateURL($sampleURL6));
-echo PHP_EOL;
-var_export(validateURL($sampleURL7));
-echo PHP_EOL;
-var_export(validateURL($sampleURL8));
-echo PHP_EOL;
-var_export(validateURL($sampleURL9));
-echo PHP_EOL;
-var_export(validateURL($sampleURL10));
-echo PHP_EOL;
+foreach ($sampleURLs as $sampleURL) {
+    $valid = true;
+    $url=$sampleURL;
+    if (substr($url, 0, 4) !== 'http') {
+        $url = 'http://' . $url;
+    }
+    if (filter_var($url, FILTER_VALIDATE_URL) === false) {
+        $valid = false;
+    }
+
+    $host = parse_url($url, PHP_URL_HOST);
+    $hosts = explode('.', $host);
+
+    foreach ($hosts as $host) {
+        if (strlen($host) < 2) {
+            $valid = false;
+        }
+        if (strpbrk($host, '_') !== false) {
+            $valid = false;
+        }
+        if (!ctype_alnum(substr($host, 0, 1))) {
+            $valid = false;
+        }
+        if (!ctype_alnum(substr($host, -1))) {
+            $valid = false;
+        }
+    }
+    var_export($valid);
+    echo PHP_EOL;
+}
 
 echo 'regex comparison' . PHP_EOL;
 //$regex = '#((http|https)://)?(\w+)/.(\w+)(\:\d*)?#';
 $regex = '#(^(http|https)://)?([^\W_ ]{2,}[\.,/%])+[^\W_ ]{2,3}(\#(\w*))?(\?.*)?(\:\d{4})?$#';
 
-
-echo preg_match($regex, $sampleURL1) . PHP_EOL;
-echo preg_match($regex, $sampleURL2) . PHP_EOL;
-echo preg_match($regex, $sampleURL3) . PHP_EOL;
-echo preg_match($regex, $sampleURL4) . PHP_EOL;
-echo preg_match($regex, $sampleURL5) . PHP_EOL;
-echo preg_match($regex, $sampleURL6) . PHP_EOL;
-echo preg_match($regex, $sampleURL7) . PHP_EOL;
-echo preg_match($regex, $sampleURL8) . PHP_EOL;
-echo preg_match($regex, $sampleURL9) . PHP_EOL;
-echo preg_match($regex, $sampleURL10) . PHP_EOL;
+foreach ($sampleURLs as $sampleURL) {
+    echo preg_match($regex, $sampleURL) . PHP_EOL;
+}
